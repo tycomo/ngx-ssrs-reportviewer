4c2db946dfe6c5e354bcec84a669e0ef9949f118
Dani Santos <daniramossantos9@gmail.com>
2022-11-18 23:17:49 +0000

    published version 11

--- projects/reportviewer/src/lib/reportviewer.component.spec.ts
+++ projects/reportviewer/src/lib/reportviewer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import {} from 'jasmine';
 import { ReportViewerComponent } from './reportviewer.component';
 
@@ -6,7 +6,7 @@ describe('ReportViewerComponent', () => {
   let component: ReportViewerComponent;
   let fixture: ComponentFixture<ReportViewerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ReportViewerComponent ]
     })
